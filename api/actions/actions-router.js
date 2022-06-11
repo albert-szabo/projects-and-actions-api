9830b17a6c32540bcd1d161fa83a1110c2036a0d
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-11 11:02:32 -0700

    Added general GET endpoint to retrieve all actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,8 +4,16 @@ const express = require('express');
 
 const Actions = require('./actions-model');
 
+
+
 const router = express.Router();
 
-
+router.get('/', (request, response, next) => {
+    Actions.get()
+        .then(actions => {
+            response.json(actions);
+        })
+        .catch(next);
+});
 
 module.exports = router;
